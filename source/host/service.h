897e5b88d8a1d337f501bfa8459160b56502c2c1
Egor Pugin <egor.pugin@gmail.com>
2018-07-13 21:40:52 +0300

    Move service.h to core.

---
+++ source/host/service.h
@@ -0,0 +1,47 @@
+//
+// PROJECT:         Aspia
+// FILE:            base/service.h
+// LICENSE:         GNU General Public License 3
+// PROGRAMMERS:     Dmitry Chapyshev (dmitry@aspia.ru)
+//
+
+#pragma once
+
+#include "base/service_impl.h"
+
+#include <qpointer.h>
+
+namespace aspia {
+
+template <class Application>
+class Service : public ServiceImpl
+{
+public:
+    explicit Service(const std::string& name, const std::string& display_name, const std::string& description)
+        : ServiceImpl(name, display_name, description)
+    {
+        // Nothing
+    }
+
+    virtual ~Service() = default;
+
+protected:
+    Application* application() const { return application_; }
+
+    // ServiceImpl implementation.
+    void createApplication(int argc, char* argv[]) override
+    {
+        application_ = new Application(argc, argv);
+    }
+
+    // ServiceImpl implementation.
+    int executeApplication() override
+    {
+        return application_->exec();
+    }
+
+private:
+    QPointer<Application> application_;
+};
+
+} // namespace aspia
